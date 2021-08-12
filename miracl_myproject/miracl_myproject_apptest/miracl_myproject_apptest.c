86e126eb7a131adc4b7f072c8d0854ec1f34fb90
大大狐狸cyberfox_meow <hitfzyangdianshi@hotmail.com>
2021-08-13 00:10:22 +0800

    add vs

---
+++ miracl_myproject/miracl_myproject_apptest/miracl_myproject_apptest.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+
+#include "miracl.h"
+
+
+/* include MIRACL system */
+
+#pragma warning(disable : 4996)
+
+int  main()
+{ /* calculate factorial of number */
+    big nf;
+    /* declare "big" variable nf */
+    int n;
+    miracl* mip = mirsys(5000, 10);  /* initialise system to base 10, 5000 digits per "big" */
+
+    nf=mirvar(1);
+
+    printf("factorial program\n");
+    printf("input number n= \n");
+    scanf("%d", &n);
+    getchar();
+
+    while (n > 1)
+        premult(nf, n--, nf);
+
+    printf("n!= \n");
+    otnum(nf, stdout); /* output result */
+
+}
